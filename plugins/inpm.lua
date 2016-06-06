4551be4edce1a1746de782a2d873568050401bfb
mahdisudo <mehdisudo@gmail.com>
2016-06-06 00:02:51 -0700

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Hi!,\nfor see bot command send #superhelp"
+		text = "سلام/nبرای در خواست گروه به ایدی  @pic5all  پیام بدید!"
      	return text
     end
 
