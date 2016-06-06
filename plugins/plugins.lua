cf0312a6f99d77720ebf765955b9f49b4de1a056
mahdisudo <mehdisudo@gmail.com>
2016-06-05 23:48:35 -0700

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -26,13 +26,13 @@ local function list_all_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  ✅ enabled, ⛔️ disabled
-    local status = '⛔️'
+    local status = '/Disable'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '✅' 
+        status = '/Enable'
       end
       nact = nact+1
     end
@@ -51,17 +51,17 @@ local function list_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  ✅ enabled, ⛔️ disabled
-    local status = '⛔️'
+    local status = '/Disable'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '✅' 
+        status = '/Enable' 
       end
       nact = nact+1
     end
-    if not only_enabled or status == '✅' then
+    if not only_enabled or status == '/Enable' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..v..'  '..status..'\n'
