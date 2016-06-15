1e9dae62d12e4671b10d8e964cef74a3b199ebb6
mahdisudo <mehdisudo@gmail.com>
2016-06-15 22:29:42 +0430

    Update cli2api.lua

--- plugins/cli2api.lua
+++ plugins/cli2api.lua
@@ -1,9 +1,9 @@
 local function run(msg, matches)
-  local token = '237645229:AAE8See6WKtCBwTOdPBF3dP74uT583qwAAw'
+  local token = '233412109:AAHReUp8w8rRClg2rUeCd3cze0gEqeQh6vc'
    local url = 'https://api.telegram.org/bot'..token..'/sendMessage?chat_id='..matches[2]..'&text='..URL.escape(matches[3])..'&parse_mode=markdown&disable_web_page_preview=true'
   https.request(url)
   print (url)
-  return 'i will sent:\n'..matches[3]..'\n with markdown to :\n '..matches[2]
+  return 'I will sent:\n'..matches[3]..'\n with markdown to :\n '..matches[2]
 
 end
 return {
