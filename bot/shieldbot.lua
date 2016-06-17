691369876b033e1e404c2fe51e75ba51db585040
mahdisudo <mehdisudo@gmail.com>
2016-06-17 13:56:12 +0430

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -289,7 +289,9 @@ function create_config( )
     "bazzar",
     "fa_voice",
     "kick_reply",
-    "png"
+    "png",
+    "hacker",
+    "tophot"
     },
     sudo_users = {24709501,155792819,83058657,232482998,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
